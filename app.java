747a4e794c4b0cc82e0549aca3e475f982524a85
zitatou <zndongmo@gmail.com>
2022-03-03 20:04:49 +0000

    deploy

--- app.java
+++ app.java
@@ -1,4 +1,7 @@
 welcome to td bank
 your financial partner
+<<<<<<< HEAD
 bug removed from the apllication
+=======
+>>>>>>> 695e237... updated
 hello
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo list of devops admin
 echo simon elvis stanley kennedy
+job well done
